fc142d4cb293191f7ed04352ae08e33d0b50e541
AntL24 <antonin.l2405@gmail.com>
2022-07-18 11:55:39 +0200

    comments deletion

--- back/models/userModel.js
+++ back/models/userModel.js
@@ -6,14 +6,12 @@ const mongoose = require('mongoose');
 require ("../mongo");
 
 //Mongoose fonctionne avec des schémas.
-//On crée un schéma pour exiger des utilisateurs un email unique et un mot de passe de minimum 8 caractères
-//avec au moins un chiffre et un caractère spécial (caractères non-verbaux valant caractère spécial).
+//On crée un schéma pour exiger des utilisateurs un email unique
 const userSchema = new mongoose.Schema({
     email: {type: String, required : true, unique : true},
     password: {type: String, required : true, minlength : 6, maxlength : 255}
 });
 //On ajoute un plugin pour vérifier que l'email est unique. 
-//?Pourquoi le message d'erreur ne s'affiche que dans la response de l'onglet network, et pas sur la page web ?
 userSchema.plugin(uniqueValidator);
 
 //Création d'un modèle qui suit UserSchema pour les utilisateurs.
